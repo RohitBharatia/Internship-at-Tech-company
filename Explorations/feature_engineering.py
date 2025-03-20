f700720b0e79d5931ab58a177c49e1e2550f9546
RohitBharatia <rohit.bharatia@gmail.com>
2025-03-20 16:47:36 +0530

    Update feature_engineering.py

--- Explorations/feature_engineering.py
+++ Explorations/feature_engineering.py
@@ -12,6 +12,8 @@
 
 import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
+import seaborn as sns
+import numpy as np
 
 # Original DataFrame
 marbles = pd.DataFrame({
@@ -45,3 +47,36 @@ print(3)
 encoded_df_final = pd.DataFrame(list(encoded_dict.items()), columns=['Colour', 'Encoded Values'])
 print("Final Encoded DataFrame:")
 print(encoded_df_final)
+
+
+
+#code below is not finished.
+tips= sns.load_dataset('tips')
+print(tips)
+encoder=OneHotEncoder(sparse_output=False, handle_unknown='ignore')
+catagorical_variables=['sex','smoker','day','time']
+encoded_variables=encoder.fit_transform(tips[catagorical_variables])
+encoded_df = pd.DataFrame(encoded_variables, columns=encoder.get_feature_names_out(catagorical_variables))
+
+# Concatenate the encoded DataFrame with the original DataFrame
+final_df = pd.concat([tips, encoded_df], axis=1)
+print(final_df)
+
+days=tips['day'].unique()
+days = days.tolist()
+print(days)
+days_df = pd.DataFrame({'Days': days})
+print(days_df)
+days_encoded = encoder.fit_transform(days_df[['Days']])
+print(days_encoded)
+days_df['encoding_key'] = np.nan  # Initialize empty column
+print(days_df)
+
+for idx, arr in enumerate(days_encoded):
+    days_df.at[idx, 'encoding_key'] = [arr]  # Assign encoded array as list
+print(days_df)
+
+#days_encoded=encoder.fit_transform(days[['days']]).toarray()
+
+
+
